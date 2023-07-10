b87f4025d7a027db5add966ea8c111e63dabfd33
Edy Galdiolo <edygaldy302@gmail.com>
2023-07-10 17:31:25 +0200

    Feat: added footer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -24,4 +24,14 @@
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora temporibus, dicta nemo aliquam totam nisi deserunt soluta quas voluptatum ab beatae praesentium necessitatibus minus, facilis illum rerum officiis accusamus dolores!</p>
     </div>
 </div>
+
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <button class="btn bg-danger">Don't click here</button>
+            </div>
+        </div>
+    </div>
+</footer>
 @endsection
